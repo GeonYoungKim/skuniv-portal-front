481f1185aa568478b0d04cfe7542f50fd7751ef2
GEONYOUNGKIM <c2619zz@naver.com>
2019-06-08 15:31:59 +0900

    react moment, dateRangePicker 추가

--- src/App.js
+++ src/App.js
@@ -4,32 +4,16 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import 'react-dates/initialize';
 
-import Main from './containers/Main';
-import Search from './containers/Search';
-import Post from './containers/Post';
-import Account from './containers/Account';
-import SignIn from './containers/SignIn';
-import SignUp from './containers/SignUp';
-import FindPassword from './containers/FindPassword';
-import FindId from './containers/FindId';
-import CreatePost from './containers/CreatePost';
 import ProfessorLecture from './containers/ProfessorLecture';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="layout ">
           <Switch>
-            <Route path="/main/:job?/:view?/:id?" component={Main} />
-            <Route path="/search/:pageNo?/:content?" component={Search} />
-            <Route path="/post/:postId?" component={Post} />
-            <Route path="/account/:job?/:view?/:id?" component={Account} />
-            <Route path="/signIn" component={SignIn} />
-            <Route path="/signUp" component={SignUp} />
-            <Route path="/findId" component={FindId} />
-            <Route path="/findPassword" component={FindPassword} />
-            <Route path="/createPost/:postType?" component={CreatePost}/>
             <Route path="/professor/lecture" component={ProfessorLecture}/>
           </Switch>
         </div>
--- src/containers/ProfessorLecture.js
+++ src/containers/ProfessorLecture.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
-import ProfessorLectureHeader from '../components/professor-lecture/js/ProfessorLectureHeader';
-import ProfessorLectureForm from '../components/professor-lecture/js/ProfessorLectureForm';
+import moment from 'moment';
+import { DateRangePicker, isInclusivelyAfterDay } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import axios from 'axios';
 
 class ProfessorLecture extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: moment(),
+            endDate: moment()
+        };
+    }
+
+    componentDidMount = () => {
+        console.log(this.state.startDate);
+        console.log(this.state.endDate);
+        axios
+        .post(
+            'http://localhost:8080/api/v1/portal/account/test', 
+            {
+                startDate: this.state.startDate,
+                endDate : this.state.endDate
+            }, 
+            {
+                headers: { 'token': localStorage.token }
+            }
+        )
+        .then((response) => {
+            if (response.status === 200) {
+                console.log(response.data)
+            }
+        });
+    }
+
     render() {
         return (
-            <main>
-                   <ProfessorLectureHeader/>
-                   <ProfessorLectureForm/>
-            </main>
+            <div style={{ height: "1000px" }}>
+
+                <DateRangePicker
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate}
+                    onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+                    focusedInput={this.state.focusedInput}
+                    onFocusChange={focusedInput => this.setState({ focusedInput })}
+                    isOutsideRange={day => isInclusivelyAfterDay(day, moment().add(1, "days"))}
+                />
+
+            </div>
         );
     }
 }
 
-export default Main;
+export default ProfessorLecture;
